ab023979a0732fbceff918486348870098c9c02b
joker69216 <hamaraad883@gmail.com>
2023-02-22 13:43:30 +0300

    fix eslint errors

--- src/redux/greetingsSlice.js
+++ src/redux/greetingsSlice.js
@@ -1,22 +1,17 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
-
-
 export const fetchGreeting = createAsyncThunk(
   'greetings/fetchGreeting',
   async () => {
     const response = await fetch('http://localhost:3000/api/greets')
       .then((resp) => {
-        console.log(resp);
         return resp.json();
       })
       .then((result) => {
-        console.log(result);
         return result;
       });
-    console.log(response);
     return response;
-  }
+  },
 );
 
 const initialState = {
