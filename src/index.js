e804ee73e6a3323c341145c81f5ed41eee724256
joker69216 <hamaraad883@gmail.com>
2023-02-22 13:51:53 +0300

    add greets to the frontend

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+
 import App from './App';
+import store from './redux/store';
+import { fetchGreeting } from './redux/greetingsSlice';
+
+store.dispatch(fetchGreeting());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+);
